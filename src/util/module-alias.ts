4714207e5a66cce483d7ad148931be6fdc4e5e20
Luan Maffra <luanmaffra@Luans-MacBook-Pro.local>
2020-10-19 22:12:49 -0300

    jest added and first test created.

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -5,5 +5,5 @@ const files = path.resolve(__dirname, '../../');
 
 moduleAlias.addAliases({
   '@src': path.join(files, 'src'),
-  '@tests': path.join(files, 'tests')
+  '@test': path.join(files, 'test')
 });
